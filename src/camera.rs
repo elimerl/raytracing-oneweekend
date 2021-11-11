b2999f17258ce50470fc572cd22c155d7b793361
elimerl <32183908+elimerl@users.noreply.github.com>
2021-11-11 15:33:40 -0800

    yay it works now

--- src/camera.rs
+++ src/camera.rs
@@ -9,34 +9,49 @@ pub struct Camera {
     horizontal: Vec3,
     vertical: Vec3,
 }
+fn deg2rad(deg: f32) -> f32 {
+    deg * std::f32::consts::PI / 180.0
+}
 impl Camera {
-    pub fn new(aspect_ratio: f32) -> Camera {
+    pub fn new(
+        lookfrom: Point3,
+        lookat: Point3,
+        vup: Vec3,
+        vfov: f32,
+        aspect_ratio: f32,
+    ) -> Camera {
         let mut cam = Camera {
             origin: Point3::new_all(0.0),
             lower_left_corner: Point3::new_all(0.0),
             horizontal: Vec3::new_all(0.0),
             vertical: Vec3::new_all(0.0),
         };
-        let viewport_height = 2.0;
+        let theta = deg2rad(vfov);
+        let h = (theta / 2.0).tan();
+        let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
+        let _focal_length = 1.0;
 
-        cam.origin = Point3::new(0.0, 0.0, 0.0);
-        cam.horizontal = Point3::new(viewport_width, 0.0, 0.0);
-        cam.vertical = Point3::new(0.0, viewport_height, 0.0);
+        let w = (lookfrom - lookat).normalize();
+        let u = vup.cross(w).normalize();
+        let v = w.cross(u);
+
+        cam.origin = lookfrom;
+        cam.horizontal = Vec3::new_all(viewport_width) * u;
+        cam.vertical = Vec3::new_all(viewport_height) * v;
         cam.lower_left_corner = cam.origin
             - cam.horizontal / Vec3::new_all(2.0)
             - cam.vertical / Vec3::new_all(2.0)
-            - Point3::new(0.0, 0.0, focal_length);
+            - w;
 
         cam
     }
-    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+    pub fn get_ray(&self, s: f32, t: f32) -> Ray {
         return Ray::new(
             self.origin,
             self.lower_left_corner
-                + Vec3::new_all(u) * self.horizontal
-                + Vec3::new_all(v) * self.vertical
+                + Vec3::new_all(s) * self.horizontal
+                + Vec3::new_all(t) * self.vertical
                 - self.origin,
         );
     }
--- src/main.rs
+++ src/main.rs
@@ -19,68 +19,93 @@ use camera::Camera;
 use hittable::HitRecord;
 use hittablelist::HittableList;
 use indicatif::{ProgressBar, ProgressStyle};
+use rand::Rng;
 use ray::Ray;
 use shapes::sphere::Sphere;
+use std::path::PathBuf;
+use structopt::StructOpt;
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "raytracer",
+    about = "A raytracer based on Ray Tracing in One Weekend"
+)]
+struct Opt {
+    /// Output file
+    #[structopt(parse(from_os_str))]
+    output: PathBuf,
+    // /// World file
+    // #[structopt(long, parse(from_os_str))]
+    // world: PathBuf,
+    /// Width of the image
+    #[structopt(short, long, default_value = "800")]
+    width: u32,
+    /// Height of the image
+    #[structopt(short, long, default_value = "450")]
+    height: u32,
+
+    /// Number of samples per pixel
+    #[structopt(short, long, default_value = "100")]
+    samples: u32,
+
+    /// Number of threads to use [default: number of cores]
+    #[structopt(short, long)]
+    threads: Option<u32>,
+}
 
 fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    if let Some(threads) = opt.threads {
+        rayon::ThreadPoolBuilder::default()
+            .num_threads(threads as usize)
+            .build_global()
+            .unwrap();
+    }
     // Image
-    let image_width = 1000;
-    let image_height = 1000;
+    let image_width = opt.width;
+    let image_height = opt.height;
     if (image_width % 2) != 0 {
         panic!("image width must be even");
     }
     if image_height % 2 != 0 {
         panic!("image height must be even");
     }
-    let samples_per_pixel = 50;
+    let samples_per_pixel = opt.samples;
     let max_depth = 50;
+
     // World
-    let mut world = HittableList::new();
-    let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Box::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
-    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.3));
-    println!(
-        "Rendering at resolution {}x{} with {} samples and max recurse {}",
-        image_width, image_height, samples_per_pixel, max_depth
+    let world = random_scene();
+
+    // Camera
+    let camera = Camera::new(
+        Point3::new(13.0, 2.0, 3.0),
+        Point3::new(0.0, 0.0, 0.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        image_width as f32 / image_height as f32,
     );
+    println!(
+        "Rendering to file {} at resolution {}x{} with {} samples and max recurse {}",
+        opt.output.to_str().unwrap(),
+        image_width,
+        image_height,
+        samples_per_pixel,
+        max_depth
+    );
+    // Progress bar
     let bar = Arc::new(Mutex::new(
         ProgressBar::new(image_width as u64 * image_height as u64).with_style(
             ProgressStyle::default_bar().template(
-                "{bar:40} [{per_sec} pixels per second] [{elapsed_precise} elapsed] [{eta} left]",
+                "{bar:40} [{per_sec} pixels per second] [{elapsed_precise} elapsed] [{eta_precise} left]",
             ),
         ),
     ));
-
-    world.add(Box::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.0),
-        0.5,
-        material_center,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
-    )));
-
-    // Camera
-    let camera = Camera::new(image_width as f32 / image_height as f32);
-
+    // Render
     let now = std::time::Instant::now();
     let image = Arc::new(Mutex::new(image::Image::new(image_width, image_height)));
     let image_clone = image.clone();
+    let bands = 8;
     rayon::scope(|s| {
-        for y in 0..image_height {
+        for band in 0..bands {
             let world = world.clone();
             let camera = camera.clone();
             let row = Arc::new(Mutex::new(vec![Color::new_all(0.0); image_width as usize]));
@@ -89,31 +114,42 @@ fn main() -> Result<()> {
             let image_clone = image_clone.clone();
 
             s.spawn(move |_| {
-                let mut row = row_clone.lock().unwrap();
-                for x in 0..image_width {
-                    let mut color = Color::new_all(0.0);
-                    for _ in 0..samples_per_pixel {
-                        let u = (x as f32 + rand::random::<f32>()) / (image_width - 1) as f32;
-                        let v = (y as f32 + rand::random::<f32>()) / (image_height - 1) as f32;
-                        let r = camera.get_ray(u, v);
-                        color = color + ray_color(r, &world, max_depth);
-                    }
-                    row[x as usize] = color;
-                    if x % 4 == 0 {
-                        bar_clone.lock().unwrap().inc(4);
+                let mut data =
+                    vec![Color::new_all(0.0); ((image_height / bands) * image_width) as usize];
+                for y in (band * (image_height / bands))..((band + 1) * (image_height / bands)) {
+                    for x in 0..image_width {
+                        let mut color = Color::new_all(0.0);
+                        for _ in 0..samples_per_pixel {
+                            let u = (x as f32 + rand::random::<f32>()) / (image_width - 1) as f32;
+                            let v = (y as f32 + rand::random::<f32>()) / (image_height - 1) as f32;
+                            let r = camera.get_ray(u, v);
+                            color = color + ray_color(r, &world, max_depth);
+                        }
+                        data[(y - (band * (image_height / bands))) as usize
+                            * image_width as usize
+                            + x as usize] = color;
+                        if x % 4 == 0 {
+                            bar_clone.lock().unwrap().inc(4);
+                        }
                     }
                 }
                 {
                     let mut img = image_clone.lock().unwrap();
-                    for x in 0..image_width {
-                        let color = row[x as usize];
+                    for y in (band * (image_height / bands))..((band + 1) * (image_height / bands))
+                    {
+                        for x in 0..image_width {
+                            let color = data[(y - (band * (image_height / bands))) as usize
+                                * image_width as usize
+                                + x as usize];
 
-                        img.set_pixel(x, y, color, samples_per_pixel);
+                            img.set_pixel(x, y, color, samples_per_pixel);
+                        }
                     }
                 }
             });
         }
     });
+    // Finish
     bar.lock().unwrap().finish();
     let elapsed = now.elapsed();
     println!(
@@ -161,4 +197,69 @@ fn hit_sphere(center: Point3, radius: f32, r: Ray) -> f32 {
     } else {
         return (-half_b - discriminant.sqrt()) / a;
     }
+}
+fn random_scene() -> HittableList {
+    let mut world = HittableList::new();
+    let ground_material = Box::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f32 = rand::random();
+            let center = Point3::new(
+                (a as f32) + 0.9 * rand::random::<f32>(),
+                0.2,
+                (b as f32) + 0.9 * rand::random::<f32>(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random() * Color::random();
+                    let sphere_material = Lambertian::new(albedo);
+                    world.add(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Box::new(sphere_material),
+                    )));
+                } else {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    let sphere_material = Metal::new(albedo, fuzz);
+                    world.add(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Box::new(sphere_material),
+                    )));
+                }
+            }
+        }
+    }
+    let material1 = Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Box::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    world
 }
